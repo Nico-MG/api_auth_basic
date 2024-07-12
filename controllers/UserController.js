b862e215c321d68186be5da023dbd880ceb1c3ab
Nico-MG <nico.jeremiah.guajardo@gmail.com>
2024-07-12 02:25:03 -0400

    no funciona el getAllUsers

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,50 +1,87 @@
-import { Router } from 'express';
-import UserService from '../services/UserService.js';
-import NumberMiddleware from '../middlewares/number.middleware.js';
-import UserMiddleware from '../middlewares/user.middleware.js';
-import AuthMiddleware from '../middlewares/auth.middleware.js';
+import { Router } from "express";
+import UserService from "../services/UserService.js";
+import NumberMiddleware from "../middlewares/number.middleware.js";
+import UserMiddleware from "../middlewares/user.middleware.js";
+import AuthMiddleware from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post('/create', async (req, res) => {
-    const response = await UserService.createUser(req);
-    res.status(response.code).json(response.message);
+router.post("/create", async (req, res) => {
+	const response = await UserService.createUser(req);
+	res.status(response.code).json(response.message);
 });
 
 router.get(
-    '/:id',
-    [
-        NumberMiddleware.isNumber,
-        UserMiddleware.isValidUserById,
-        AuthMiddleware.validateToken,
-        UserMiddleware.hasPermissions
-    ],
-    async (req, res) => {
-        const response = await UserService.getUserById(req.params.id);
-        res.status(response.code).json(response.message);
-    });
+	"/:id/user",
+	[
+		NumberMiddleware.isNumber,
+		UserMiddleware.isValidUserById,
+		AuthMiddleware.validateToken,
+		UserMiddleware.hasPermissions,
+	],
+	async (req, res) => {
+		const response = await UserService.getUserById(req.params.id);
+		res.status(response.code).json(response.message);
+	},
+);
 
-router.put('/:id', [
-        NumberMiddleware.isNumber,
-        UserMiddleware.isValidUserById,
-        AuthMiddleware.validateToken,
-        UserMiddleware.hasPermissions,
-    ],
-    async(req, res) => {
-        const response = await UserService.updateUser(req);
-        res.status(response.code).json(response.message);
-    });
+router.put(
+	"/:id",
+	[
+		NumberMiddleware.isNumber,
+		UserMiddleware.isValidUserById,
+		AuthMiddleware.validateToken,
+		UserMiddleware.hasPermissions,
+	],
+	async (req, res) => {
+		const response = await UserService.updateUser(req);
+		res.status(response.code).json(response.message);
+	},
+);
 
-router.delete('/:id',
-    [
-        NumberMiddleware.isNumber,
-        UserMiddleware.isValidUserById,
-        AuthMiddleware.validateToken,
-        UserMiddleware.hasPermissions,
-    ],
-    async (req, res) => {
-       const response = await UserService.deleteUser(req.params.id);
-       res.status(response.code).json(response.message);
-    });
+router.delete(
+	"/:id",
+	[
+		NumberMiddleware.isNumber,
+		UserMiddleware.isValidUserById,
+		AuthMiddleware.validateToken,
+		UserMiddleware.hasPermissions,
+	],
+	async (req, res) => {
+		const response = await UserService.deleteUser(req.params.id);
+		res.status(response.code).json(response.message);
+	},
+);
+
+router.get("/getAllUsers", [
+	AuthMiddleware.validateToken,
+	UserMiddleware.hasPermissions,
+], async (req, res) => {
+	const response = await UserService.getAllUsers();
+	res.status(response.code).json(response.message);
+});
+
+router.get("/findUsers", [
+    NumberMiddleware.isNumber,
+    UserMiddleware.isValidUserById,
+    AuthMiddleware.validateToken,
+    UserMiddleware.hasPermissions,
+], async (req, res) => {
+    req.query.status = req.query.status ?? true;
+    req.query.from = req.query.from ?? "1900-01-01";
+    req.query.to = req.query.to ?? "2100-01-01";
+    const response = await UserService.findUsers(req.query);
+    res.status(response.code).json(response.message);
+});
+
+router.post('/bulkCreate',[
+	NumberMiddleware.isNumber,
+	UserMiddleware.isValidUserById,
+	AuthMiddleware.validateToken,
+	UserMiddleware.hasPermissions,
+],async (req, res) => {
+	const response = await UserService.bulkCreate(req.body);
+	res.status(response.code).json(response.message);
+});
 
 export default router;
--- services/UserService.js
+++ services/UserService.js
@@ -1,110 +1,142 @@
-import db from '../dist/db/models/index.js';
-import bcrypt from 'bcrypt';
+import db from "../dist/db/models/index.js";
+import bcrypt from "bcrypt";
 
 const createUser = async (req) => {
-    const {
-        name,
-        email,
-        password,
-        password_second,
-        cellphone
-    } = req.body;
-    if (password !== password_second) {
-        return {
-            code: 400,
-            message: 'Passwords do not match'
-        };
-    }
-    const user = await db.User.findOne({
-        where: {
-            email: email
-        }
-    });
-    if (user) {
-        return {
-            code: 400,
-            message: 'User already exists'
-        };
-    }
+	const { name, email, password, password_second, cellphone } = req.body;
+	if (password !== password_second) {
+		return {
+			code: 400,
+			message: "Passwords do not match",
+		};
+	}
+	const user = await db.User.findOne({
+		where: {
+			email: email,
+		},
+	});
+	if (user) {
+		return {
+			code: 400,
+			message: "User already exists",
+		};
+	}
 
-    const encryptedPassword = await bcrypt.hash(password, 10);
+	const encryptedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await db.User.create({
-        name,
-        email,
-        password: encryptedPassword,
-        cellphone,
-        status: true
-    });
-    return {
-        code: 200,
-        message: 'User created successfully with ID: ' + newUser.id,
-    }
+	const newUser = await db.User.create({
+		name,
+		email,
+		password: encryptedPassword,
+		cellphone,
+		status: true,
+	});
+	return {
+		code: 200,
+		message: `User created successfully with ID: ${newUser.id}`,
+	};
 };
 
 const getUserById = async (id) => {
-    return {
-        code: 200,
-        message: await db.User.findOne({
-            where: {
-                id: id,
-                status: true,
-            }
-        })
-    };
-}
+	return {
+		code: 200,
+		message: await db.User.findOne({
+			where: {
+				id: id,
+				status: true,
+			},
+		}),
+	};
+};
 
 const updateUser = async (req) => {
-    const user = db.User.findOne({
-        where: {
-            id: req.params.id,
-            status: true,
-        }
-    });
-    const payload = {};
-    payload.name = req.body.name ?? user.name;
-    payload.password = req.body.password ? await bcrypt.hash(req.body.password, 10) : user.password;
-    payload.cellphone = req.body.cellphone ?? user.cellphone;
-    await db.User.update(payload, {
-        where: {
-            id: req.params.id
-        }
-
-    });
-    return {
-        code: 200,
-        message: 'User updated successfully'
-    };
-}
+	const user = db.User.findOne({
+		where: {
+			id: req.params.id,
+			status: true,
+		},
+	});
+	const payload = {};
+	payload.name = req.body.name ?? user.name;
+	payload.password = req.body.password
+		? await bcrypt.hash(req.body.password, 10)
+		: user.password;
+	payload.cellphone = req.body.cellphone ?? user.cellphone;
+	await db.User.update(payload, {
+		where: {
+			id: req.params.id,
+		},
+	});
+	return {
+		code: 200,
+		message: "User updated successfully",
+	};
+};
 
 const deleteUser = async (id) => {
-    /* await db.User.destroy({
+	/* await db.User.destroy({
         where: {
             id: id
         }
     }); */
-    const user = db.User.findOne({
-        where: {
-            id: id,
-            status: true,
-        }
-    });
-    await  db.User.update({
-        status: false
-    }, {
-        where: {
-            id: id
-        }
-    });
-    return {
-        code: 200,
-        message: 'User deleted successfully'
-    };
-}
+	const user = db.User.findOne({
+		where: {
+			id: id,
+			status: true,
+		},
+	});
+	await db.User.update(
+		{
+			status: false,
+		},
+		{
+			where: {
+				id: id,
+			},
+		},
+	);
+	return {
+		code: 200,
+		message: "User deleted successfully",
+	};
+};
+
+const getAllUsers = async () => {
+	return {
+		code: 200,
+		message: await db.User.findAll({
+			where: {
+				status: true,
+			},
+		}),
+	};
+};
+
+const findUsers = async (query) => {
+	return {
+		code: 200,
+		message: await db.User.findAll({
+			where: {
+				...query,
+				status: true,
+			},
+		}),
+	};
+};
+
+const bulkCreate = async (data) => {
+	const users = await db.User.bulkCreate(data);
+	return {
+		code: 200,
+		message: users,
+	};
+};
 
 export default {
-    createUser,
-    getUserById,
-    updateUser,
-    deleteUser,
-}
+	createUser,
+	getUserById,
+	updateUser,
+	deleteUser,
+	getAllUsers,
+	findUsers,
+	bulkCreate,
+};
