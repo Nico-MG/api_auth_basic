769bc902a9ac79a7400bd2df82ce19b4e0a62727
Nico-MG <nico.jeremiah.guajardo@gmail.com>
2024-07-15 00:19:57 -0400

    fix create errors

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,117 +9,131 @@ import AuthMiddleware from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/create", async (req, res) => {
-  try {
-    const response = await UserService.createUser(req);
-    return res.status(201).json({ message: `User created successfully with ID: ${response.id}` });
-  } catch (error) {
-    if (error instanceof UserExists) {
-      return res.status(400).json({ message: error.message });
-    }
-    if (error instanceof PasswordNotMatch) {
-      return res.status(400).json({ message: error.message });
-    }
-    return res.status(500).json({ message: "Error de servidor" });
-  }
-});
+router.post(
+	"/create",
+	[AuthMiddleware.validateToken, UserMiddleware.hasPermissions],
+	async (req, res) => {
+		try {
+			const response = await UserService.createUser(req);
+			return res
+				.status(201)
+				.json({ message: `User created successfully with ID: ${response.id}` });
+		} catch (error) {
+			if (error instanceof UserExists) {
+				return res.status(400).json({ message: error.message });
+			}
+			if (error instanceof PasswordNotMatch) {
+				return res.status(400).json({ message: error.message });
+			}
+			return res.status(500).json({ message: "Error de servidor" });
+		}
+	},
+);
 
-router.post("/bulkCreate", async (req, res) => {
-  try {
-    const response = await UserService.bulkCreateUsers(req);
-    return res.status(201).json({ message: `${response.usersInsertedCount} users created successfully, ${response.usersNotInsertedCount} users not inserted` });
-  } catch (error) {
-    return res.status(500).json({ message: "Error de servidor" });
-  }
-});
-
-router.get(
-  "/getAllUsers",
-  [AuthMiddleware.validateToken, UserMiddleware.hasPermissions],
-  async (req, res) => {
-    try {
-      const response = await UserService.getAllUsers();
-      return res.status(200).json({ message: response });
-    } catch (error) {
-      return res.status(500).json({ message: "Error de servidor" });
-    }
-  }
+router.post(
+	"/bulkCreate",
+	[AuthMiddleware.validateToken, UserMiddleware.hasPermissions],
+	async (req, res) => {
+		try {
+			const response = await UserService.bulkCreateUsers(req);
+			return res
+				.status(201)
+				.json({
+					message: `${response.usersInsertedCount} users created successfully, ${response.usersNotInsertedCount} users not inserted`,
+				});
+		} catch (error) {
+			return res.status(500).json({ message: "Error de servidor" });
+		}
+	},
 );
 
 router.get(
-    "/findUsers",
-    [AuthMiddleware.validateToken, UserMiddleware.hasPermissions],
-    async (req, res) => {
-        try {
-            const response = await UserService.findUsers(req);
-            return res.status(200).json({ message: response });
-        } catch (error) {
-            return res.status(500).json({ message: "Error de servidor" });
-        }
-      }
-)
+	"/getAllUsers",
+	[AuthMiddleware.validateToken, UserMiddleware.hasPermissions],
+	async (req, res) => {
+		try {
+			const response = await UserService.getAllUsers();
+			return res.status(200).json({ message: response });
+		} catch (error) {
+			return res.status(500).json({ message: "Error de servidor" });
+		}
+	},
+);
 
 router.get(
-  "/:id",
-  [
-    NumberMiddleware.isNumber,
-    UserMiddleware.isValidUserById,
-    AuthMiddleware.validateToken,
-    UserMiddleware.hasPermissions,
-  ],
-  async (req, res) => {
-    try {
-      const response = await UserService.getUserById(req.params.id);
-      return res.status(200).json({ message: response });
-    } catch (error) {
-      if (error instanceof UserNotFound) {
-        return res.status(404).json({ message: error.message });
-      }
-      return res.status(500).json({ message: "Error de servidor" });
-    }
-  }
+	"/findUsers",
+	[AuthMiddleware.validateToken, UserMiddleware.hasPermissions],
+	async (req, res) => {
+		try {
+			const response = await UserService.findUsers(req);
+			return res.status(200).json({ message: response });
+		} catch (error) {
+			return res.status(500).json({ message: "Error de servidor" });
+		}
+	},
+);
+
+router.get(
+	"/:id",
+	[
+		NumberMiddleware.isNumber,
+		UserMiddleware.isValidUserById,
+		AuthMiddleware.validateToken,
+		UserMiddleware.hasPermissions,
+	],
+	async (req, res) => {
+		try {
+			const response = await UserService.getUserById(req.params.id);
+			return res.status(200).json({ message: response });
+		} catch (error) {
+			if (error instanceof UserNotFound) {
+				return res.status(404).json({ message: error.message });
+			}
+			return res.status(500).json({ message: "Error de servidor" });
+		}
+	},
 );
 
 router.put(
-  "/:id",
-  [
-    NumberMiddleware.isNumber,
-    UserMiddleware.isValidUserById,
-    AuthMiddleware.validateToken,
-    UserMiddleware.hasPermissions,
-  ],
-  async (req, res) => {
-    try {
-      const response = await UserService.updateUser(req);
-      return res.status(200).json({ message: "User updated successfully" });
-    } catch (error) {
-      if (error instanceof UserNotFound) {
-        return res.status(404).json({ message: error.message });
-      }
-      return res.status(500).json({ message: "Error de servidor" });
-    }
-  }
+	"/:id",
+	[
+		NumberMiddleware.isNumber,
+		UserMiddleware.isValidUserById,
+		AuthMiddleware.validateToken,
+		UserMiddleware.hasPermissions,
+	],
+	async (req, res) => {
+		try {
+			const response = await UserService.updateUser(req);
+			return res.status(200).json({ message: "User updated successfully" });
+		} catch (error) {
+			if (error instanceof UserNotFound) {
+				return res.status(404).json({ message: error.message });
+			}
+			return res.status(500).json({ message: "Error de servidor" });
+		}
+	},
 );
 
 router.delete(
-  "/:id",
-  [
-    NumberMiddleware.isNumber,
-    UserMiddleware.isValidUserById,
-    AuthMiddleware.validateToken,
-    UserMiddleware.hasPermissions,
-  ],
-  async (req, res) => {
-    try {
-      const response = await UserService.deleteUser(req.params.id);
-      return res.status(200).json({ message: "User deleted successfully" });
-    } catch (error) {
-      if (error instanceof UserNotFound) {
-        return res.status(404).json({ message: error.message });
-      }
-      return res.status(500).json({ message: "Error de servidor" });
-    }
-  }
+	"/:id",
+	[
+		NumberMiddleware.isNumber,
+		UserMiddleware.isValidUserById,
+		AuthMiddleware.validateToken,
+		UserMiddleware.hasPermissions,
+	],
+	async (req, res) => {
+		try {
+			const response = await UserService.deleteUser(req.params.id);
+			return res.status(200).json({ message: "User deleted successfully" });
+		} catch (error) {
+			if (error instanceof UserNotFound) {
+				return res.status(404).json({ message: error.message });
+			}
+			return res.status(500).json({ message: "Error de servidor" });
+		}
+	},
 );
 
 export default router;
--- services/UserService.js
+++ services/UserService.js
@@ -6,158 +6,157 @@ import bcrypt from "bcrypt";
 import { Op } from "sequelize";
 
 const createUser = async (req) => {
-    const { name, email, password, password_second, cellphone } = req.body;
-    if (password !== password_second) {
-        throw new PasswordNotMatch();
-    }
-    const user = await db.User.findOne({
-        where: {
-            email: email,
-        },
-    });
-    if (user) {
-        throw new UserExists();
-    }
+	const { name, email, password, password_second, cellphone } = req.body;
+	if (password !== password_second) {
+		throw new PasswordNotMatch();
+	}
+	const user = await db.User.findOne({
+		where: {
+			email: email,
+		},
+	});
+	if (user) {
+		throw new UserExists();
+	}
 
-    const encryptedPassword = await bcrypt.hash(password, 10);
+	const encryptedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await db.User.create({
-        name,
-        email,
-        password: encryptedPassword,
-        cellphone,
-        status: true,
-    });
-    return newUser;
+	const newUser = await db.User.create({
+		name,
+		email,
+		password: encryptedPassword,
+		cellphone,
+		status: true,
+	});
+	return newUser;
 };
 
 const bulkCreateUsers = async (req) => {
-    const users = req.body;
-    let usersInsertedCount = 0;
-    let usersNotInsertedCount = 0;
+	const users = req.body;
+	let usersInsertedCount = 0;
+	let usersNotInsertedCount = 0;
 
-    for (const user of users) {
-        const userReq = { body: user };
-        try {
-            await createUser(userReq);
-            usersInsertedCount++;
-        } catch (error) {
-            usersNotInsertedCount++;
-        }
-    }
-    return {usersInsertedCount, usersNotInsertedCount};
+	for (const user of users) {
+		const userReq = { body: user };
+		try {
+			await createUser(userReq);
+			usersInsertedCount++;
+		} catch (error) {
+			usersNotInsertedCount++;
+		}
+	}
+	return { usersInsertedCount, usersNotInsertedCount };
 };
 
 const getUserById = async (id) => {
-
-    const user = await db.User.findOne({
-            where: {
-                id: id,
-                status: true,
-            },
-        });
-    if (!user) {
-        throw new UserNotFound();
-    }
-    return user;
+	const user = await db.User.findOne({
+		where: {
+			id: id,
+			status: true,
+		},
+	});
+	if (!user) {
+		throw new UserNotFound();
+	}
+	return user;
 };
 
 const getAllUsers = async () => {
-    return await db.User.findAll({
-            where: {
-                status: true,
-            },
-        });
+	return await db.User.findAll({
+		where: {
+			status: true,
+		},
+	});
 };
 
 const findUsers = async (req) => {
-    const { active, name, login_after_date, login_before_date } = req.query;
+	const { active, name, login_after_date, login_before_date } = req.query;
 
-    const userQuery = {};
-    const sessionQuery = {};
+	const userQuery = {};
+	const sessionQuery = {};
 
-    active && (userQuery.status = active === "true" ? true : false);
-    name && (userQuery.name = { [Op.like]: `%${name}%` });
+	active && (userQuery.status = active === "true" ? true : false);
+	name && (userQuery.name = { [Op.like]: `%${name}%` });
 
-    login_after_date &&
-        (sessionQuery.createdAt = {
-            ...sessionQuery.createdAt,
-            [Op.gte]: new Date(login_after_date),
-        });
-    login_before_date &&
-        (sessionQuery.createdAt = {
-            ...sessionQuery.createdAt,
-            [Op.lte]: new Date(login_before_date),
-        });
+	login_after_date &&
+		(sessionQuery.createdAt = {
+			...sessionQuery.createdAt,
+			[Op.gte]: new Date(login_after_date),
+		});
+	login_before_date &&
+		(sessionQuery.createdAt = {
+			...sessionQuery.createdAt,
+			[Op.lte]: new Date(login_before_date),
+		});
 
-    const includeObj =
-        Object.keys(sessionQuery).length > 0
-            ? [
-                  {
-                      model: db.Session,
-                      where: sessionQuery,
-                      attributes: [],
-                  },
-              ]
-            : [];
+	const includeObj =
+		Object.keys(sessionQuery).length > 0
+			? [
+					{
+						model: db.Session,
+						where: sessionQuery,
+						attributes: [],
+					},
+				]
+			: [];
 
-    return await db.User.findAll({
-            where: userQuery,
-            include: includeObj,
-        });
+	return await db.User.findAll({
+		where: userQuery,
+		include: includeObj,
+	});
 };
 
 const updateUser = async (req) => {
-    const user = db.User.findOne({
-        where: {
-            id: req.params.id,
-            status: true,
-        },
-    });
-    if (!user) {
-        throw new UserNotFound();
-    }
-    const payload = {};
-    payload.name = req.body.name ?? user.name;
-    payload.password = req.body.password
-        ? await bcrypt.hash(req.body.password, 10)
-        : user.password;
-    payload.cellphone = req.body.cellphone ?? user.cellphone;
-    await db.User.update(payload, {
-        where: {
-            id: req.params.id,
-        },
-    });
+	const user = db.User.findOne({
+		where: {
+			id: req.params.id,
+			status: true,
+		},
+	});
+	if (!user) {
+		throw new UserNotFound();
+	}
+	const payload = {};
+	payload.name = req.body.name ?? user.name;
+	payload.password = req.body.password
+		? await bcrypt.hash(req.body.password, 10)
+		: user.password;
+	payload.cellphone = req.body.cellphone ?? user.cellphone;
+	await db.User.update(payload, {
+		where: {
+			id: req.params.id,
+		},
+	});
 };
 
 const deleteUser = async (id) => {
-    const user = db.User.findOne({
-        where: {
-            id: id,
-            status: true,
-        },
-    });
-    if (!user) {
-        throw new UserNotFound();
-    }
-    await db.User.update(
-        {
-            status: false,
-        },
-        {
-            where: {
-                id: id,
-            },
-        }
-    );
+	const user = db.User.findOne({
+		where: {
+			id: id,
+			status: true,
+		},
+	});
+	if (!user) {
+		throw new UserNotFound();
+	}
+	await db.User.update(
+		{
+			status: false,
+		},
+		{
+			where: {
+				id: id,
+			},
+		},
+	);
 };
 
 export default {
-    createUser,
-    bulkCreateUsers,
-    getUserById,
-    getAllUsers,
-    findUsers,
-    updateUser,
-    deleteUser,
+	createUser,
+	bulkCreateUsers,
+	getUserById,
+	getAllUsers,
+	findUsers,
+	updateUser,
+	deleteUser,
 };
